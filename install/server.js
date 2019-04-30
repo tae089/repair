2a7471ff220ac2d7eeee35dc90a9c1101430ae24
tae089 <amager084@gmail.com>
2019-04-30 18:27:32 +0700

    update server.js

--- install/server.js
+++ install/server.js
@@ -2,14 +2,19 @@ const io = require('socket.io')(8080);
 
 io.on('connection', (socket) => {
     console.log('Connected');
+    socket.on('show_card', (message) => {
+        console.log(message);
+        io.sockets.emit('show_card', 'Show Data Now!');
+    });
+
     socket.on('new_card', (message) => {
         console.log(message);
-        //socket.broadcast.emit('show', 'Show Data Now!');
+        io.sockets.emit('show_card', 'Show Data Now!');
     });
 
     socket.on('delect_card', (message) => {
         console.log(message);
-        io.emit('show_card', 'Delect Data Now!');
+        io.sockets.emit('show_card', 'Show Delect Data Now!');
     });
     //Whenever someone disconnects this piece of code executed
     // socket.on('disconnect', () => {
